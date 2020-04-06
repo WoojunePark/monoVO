60bf04c6923aa55700dc804479bef9648f7398d6
WOOJUNE PARK <woojunepark@gmail.com>
2020-04-06 10:08:17 +0900

    tinkering

--- test.py
+++ test.py
@@ -1,41 +1,45 @@
-import numpy as np 
+import numpy as np
 import cv2
 import os
 
 from visual_odometry import PinholeCamera, VisualOdometry
 
-
 cam = PinholeCamera(1241.0, 376.0, 718.8560, 718.8560, 607.1928, 185.2157)
 vo = VisualOdometry(cam, '/database3/KITTI_odometry/dataset/poses/00.txt')
 
-traj = np.zeros((600,600,3), dtype=np.uint8)
+traj = np.zeros((600, 600, 3), dtype=np.uint8)
 
 for img_id in range(4541):
-	img = cv2.imread('/database3/KITTI_odometry/dataset/sequences/00/image_2/'+str(img_id).zfill(6)+'.png', 0)
+	img = cv2.imread('/database3/KITTI_odometry/dataset/sequences/00/image_2/' + str(img_id).zfill(6) + '.png', 0)
 
 	vo.update(img, img_id)
 
 	cur_t = vo.cur_t
+	# print("\ncur_t\n", cur_t)
+	# print("cur_R\n", vo.cur_R)
+	# print("px_ref\n", vo.px_ref)
+	# print("px_cur\n", vo.px_cur)
 
 	if img_id > 2:
 		x, y, z = cur_t[0], cur_t[1], cur_t[2]
 	else:
 		x, y, z = 0., 0., 0.
 
-	draw_x, draw_y = int(x)+290, int(z)+90
-	true_x, true_y = int(vo.trueX)+290, int(vo.trueZ)+90
+	draw_x, draw_y = int(x) + 290, int(z) + 90
+	true_x, true_y = int(vo.trueX) + 290, int(vo.trueZ) + 90
 
-	cv2.circle(traj, (draw_x,draw_y), 1, (img_id*255/4540,255-img_id*255/4540,0), 1)
-	cv2.circle(traj, (true_x,true_y), 1, (0,0,255), 2)
-	cv2.rectangle(traj, (10, 20), (600, 60), (0,0,0), -1)
-	text = "Coordinates: x=%2fm y=%2fm z=%2fm"%(x,y,z)
-	cv2.putText(traj, text, (20,40), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1, 8)
-	path = '/home/wjpark/Github/monoVO/output'
+	# traj line color green to blue
+	cv2.circle(traj, (draw_x, draw_y), 1, (img_id * 255 / 4540, 255 - img_id * 255 / 4540, 0), 1)
+	cv2.circle(traj, (true_x, true_y), 1, (0, 0, 255), 2)
+	cv2.rectangle(traj, (10, 20), (600, 60), (0, 0, 0), -1)
+	text = "Coordinates: x=%2fm y=%2fm z=%2fm" % (x, y, z)
+	cv2.putText(traj, text, (20, 40), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 1, 8)
+	path = '/home/wjpark/Github/monoVO/output/'
 
-	if img_id % 10 == 0:
-		cv2.imwrite(os.path.join(path, 'Road facing camera_', str(img_id), '_.jpg'), img)
-		cv2.imwrite(os.path.join(path, 'Trajectory_', str(img_id), '_.jpg'), traj)
-
-		print("img_id :", img_id)
+	# if img_id % 10 == 0:
+	#     cv2.imwrite(os.path.join(path, 'Road facing camera_', str(img_id), '_.jpg'), img)
+	#     cv2.imwrite(os.path.join(path, 'Trajectory_', str(img_id), '_.jpg'), traj)
+	#
+	# print("img_id :", img_id)
 
 cv2.imwrite('/home/wjpark/Github/monoVO/map.png', traj)
